d9d1c3cff5cab9bda08a95d8cc83225ee9868eb7
Razvan Luncan <razvan.luncan@tss-yonder.com>
2021-12-21 11:22:06 +0200

    Add filter on employee toolbar
    Add filter on employee toolbar

--- src/pages/employees/EmployeesList.svelte
+++ src/pages/employees/EmployeesList.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
-  import {DataTable, DataTableSkeleton, Toolbar, ToolbarContent, ToolbarSearch } from 'carbon-components-svelte';
+  import {
+    DataTable,
+    DataTableSkeleton,
+    Toolbar,
+    ToolbarContent,
+    ToolbarSearch,
+  } from 'carbon-components-svelte';
   import { onMount } from 'svelte';
   import type { EmployeeDto } from '../../apis/employee.api';
   import { fetchEmployees } from '../../apis/employee.api';
@@ -8,11 +14,11 @@
 
   let headers = [
     { key: 'yoShort', value: 'YO' },
-    { key: 'firstName', value: 'First name'},
+    { key: 'firstName', value: 'First name' },
     { key: 'lastName', value: 'Last name' },
-    { key: 'birthDate', value: 'Birthdate'},
-    { key: 'hireDate', value: 'Hire date'},
-    { key: 'position', value: 'Role'},
+    { key: 'birthDate', value: 'Birthdate' },
+    { key: 'hireDate', value: 'Hire date' },
+    { key: 'position', value: 'Role' },
   ];
 
   onMount(async () => {
@@ -21,23 +27,35 @@
 
   let value = '';
 
-  $: filteredEmployees = employees === undefined ? 
-    employees : employees.filter((employee) => {
-              if (value.trim().length === 0) return employee;
-              return (employee.yoShort.toLowerCase().includes(value) ||
-              employee.firstName.toLowerCase().concat(' ', employee.lastName.toLowerCase()).includes(value) ||
-              employee.lastName.toLowerCase().concat(' ', employee.firstName.toLowerCase()).includes(value) ||
-              employee.birthDate.toLowerCase().includes(value) || 
-              employee.hireDate.toLowerCase().includes(value) ||
-              employee.position.toLowerCase().includes(value));
-            });
-
+  $: filteredEmployees =
+    employees === undefined
+      ? employees
+      : employees.filter((employee) => {
+          if (value.trim().length === 0) return employee;
+          return (
+            employee.yoShort.toLowerCase().includes(value.toLowerCase()) ||
+            employee.firstName
+              .toLowerCase()
+              .concat(' ', employee.lastName.toLowerCase())
+              .includes(value.toLowerCase()) ||
+            employee.lastName
+              .toLowerCase()
+              .concat(' ', employee.firstName.toLowerCase().toLowerCase())
+              .includes(value.toLowerCase()) ||
+            employee.birthDate.toLowerCase().includes(value.toLowerCase()) ||
+            employee.hireDate.toLowerCase().includes(value.toLowerCase()) ||
+            employee.position.toLowerCase().includes(value.toLowerCase())
+          );
+        });
 </script>
 
 {#if !employees}
   <DataTableSkeleton showToolbar={false} size="compact" {headers} rows={20} />
 {:else}
-  <ToolbarSearch bind:value placeholder="Search here for names, birth dates, hire dates and so on."/>
+  <ToolbarSearch
+    bind:value
+    placeholder="Search here for names, birth dates, hire dates and so on."
+  />
   <DataTable
     size="compact"
     sortable={true}
