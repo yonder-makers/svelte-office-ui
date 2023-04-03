22497a09ff584bf64d2443cb54999ea7980a35f4
AGrigoruta <alexander.grigoruta@gmail.com>
2023-04-03 17:09:31 +0300

    redesigned the employee page into a card list, added employee photos as well

---
+++ src/components/Image.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let src;
+  export let alt;
+  export let fallback;
+  import { onMount } from 'svelte';
+  let loaded = false;
+  let thisImage;
+  onMount(() => {
+    thisImage.onload = () => {
+      loaded = true;
+    };
+  });
+</script>
+
+<img
+  {src}
+  {alt}
+  class:loaded
+  bind:this={thisImage}
+  loading="lazy"
+  onerror="this.src='{fallback}'"
+/>
+
+<style>
+  img {
+    height: 150px;
+    width: 150px;
+    opacity: 0;
+    transition: opacity 1200ms ease-out;
+    border: 1px solid transparent;
+    border-radius: 150px;
+    object-fit: cover;
+  }
+  img.loaded {
+    opacity: 1;
+  }
+</style>
---
+++ src/pages/employees/EmployeesSkeletonList.svelte
@@ -0,0 +1,68 @@
+<script>
+  import {
+    SkeletonPlaceholder,
+    SkeletonText,
+    Tile,
+  } from 'carbon-components-svelte';
+  import { onMount } from 'svelte';
+
+  export let elements;
+  let fakeEmployeeList = [];
+
+  onMount(() => {
+    fakeEmployeeList = new Array(elements);
+  });
+</script>
+
+<SkeletonPlaceholder
+  style="height: 3rem; width: 100%; margin-bottom: 0.5rem;"
+/>
+<section class="employee__container">
+  {#each fakeEmployeeList as _}
+    <Tile class="employee--placeholder">
+      <SkeletonPlaceholder class="employee__image-placeholder" />
+      <SkeletonText width="50%" />
+      <SkeletonText width="50%" />
+      <div class="employee__info--secondary">
+        <SkeletonText width="100%" />
+        <SkeletonText width="100%" />
+        <SkeletonText width="100%" />
+        <SkeletonText width="100%" />
+        <SkeletonText width="100%" />
+      </div>
+    </Tile>
+  {/each}
+</section>
+
+<style>
+  .employee__container {
+    display: flex;
+    gap: 25px 15px;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .employee__info--secondary {
+    width: 100%;
+    height: 150px;
+    margin-top: 1rem;
+    background-color: rgba(100, 100, 100, 0.1);
+    border: 1px solid rgba(100, 100, 100, 0.2);
+    border-radius: 5px;
+    padding: 10px;
+  }
+
+  :global(.employee--placeholder) {
+    width: 300px;
+    height: 360px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  :global(.employee__image-placeholder) {
+    width: 150px;
+    height: 150px;
+    margin-bottom: 10px;
+  }
+</style>
